7d0f30effabf4fed7b528fc27365d9fde909fbfa
Gilmar Ferreira Terres Correa <gilmarftc@gmail.com>
2026-01-04 21:42:21 -0300

    identação blocos

---
+++ identacao_blocos.py
@@ -0,0 +1,14 @@
+def sacar(valor):
+ saldo = 500
+
+ if saldo >= valor:
+  print("valor sacado")
+  print("retirar o seu dinheiro na boca do caixa")
+
+ print("obrigado por ser cliente")
+
+def depositar(valor):
+ saldo = 500
+ saldo += valor
+
+sacar(10)
