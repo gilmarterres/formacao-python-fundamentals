e3730973194e155b9615136d42fb0d3403f8bdf0
Gilmar <gilmarftc@gmail.com>
2026-01-07 22:39:38 -0300

    estrutura repeticao continuacao

--- estrutura_repeticao_for.py
+++ estrutura_repeticao_for.py
@@ -1,10 +1,14 @@
-texto = input("Informe um texto!")
-
+# texto = input("Informe um texto!")
+texto = ""
 VOGAIS = "AEIOU"
 
 for l in texto:
     if l.upper() in VOGAIS:
-        print(l, end="")
+        print(l.upper(), end="")
 else:
     print() #adiciona quebea de linha
-    print("Executa no final")
+    #print("Executa no final")
+
+
+for numero in range(0, 51, 5):
+    print(numero, end="-")
