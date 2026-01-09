499a2b41d475c784004528fe274f8bdbe050a686
Gilmar <gilmarftc@gmail.com>
2026-01-09 12:50:24 -0300

    desafio 2 e 3

--- desafio_2.py
+++ desafio_2.py
@@ -1,6 +1,17 @@
 def verificador_ano_bissexto():
     ano = int(input())
+    if (ano % 4 == 0) and (ano % 100 != 0):
+        print("SIM")
+    elif ano % 400 == 0:
+        print("SIM")
+    else:
+        print("NÃO")
 
-# TODO: Verifique se o ano é bissexto utilizando uma estrutura de controle condicional, como if/else:
+# TOD: Verifique se o ano é bissexto utilizando uma estrutura de controle condicional, como if/else:
+# REGRA:
+
+# Um ano é bissexto se:
+# 1. Ele é divisível por 4 e não é divisível por 100.
+# 2. Ou ele é divisível por 400.
 
 verificador_ano_bissexto()
