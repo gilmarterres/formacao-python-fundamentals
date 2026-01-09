71566cde0e8c05b58cbdba527e28fb6638d1ecd7
Gilmar <gilmarftc@gmail.com>
2026-01-08 23:12:08 -0300

    desafios

---
+++ desafio_2.py
@@ -0,0 +1,6 @@
+def verificador_ano_bissexto():
+    ano = int(input())
+
+# TODO: Verifique se o ano é bissexto utilizando uma estrutura de controle condicional, como if/else:
+
+verificador_ano_bissexto()
