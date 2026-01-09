08153d41d8aab4cd5159ae06ad9b66dedddf8dc6
Gilmar <gilmarftc@gmail.com>
2026-01-08 23:04:32 -0300

    strings 2 a 4

---
+++ string_03.py
@@ -0,0 +1,10 @@
+nome = "Gilmar Ferreira Terres Correa"
+
+print(nome[0])
+print(nome[-2])
+print(nome[:6])
+print(nome[7:])
+print(nome[7:15])
+print(nome[7:15:2])
+print(nome[:])
+print(nome[::-1])
