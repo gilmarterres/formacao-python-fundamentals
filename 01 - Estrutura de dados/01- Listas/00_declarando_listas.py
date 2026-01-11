2d6226112ddf0b1bc5b7eecc7d9b0d0164e25ac1
Gilmar Ferreira Terres Correa <gilmarftc@gmail.com>
2026-01-11 20:44:15 -0300

    listas

---
+++ 01 - Estrutura de dados/01- Listas/00_declarando_listas.py
@@ -0,0 +1,14 @@
+frutas = ["Laranja", "maca", "uva"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("Python")
+print(letras)
+
+numeros = list(range(10))
+print(numeros)
+
+carro = ["Ferrari", "F8", 42000000, 2020, 2900, "São Paulo", True]
+print(carro)
---
+++ 01 - Estrutura de dados/01- Listas/12_index.py
@@ -0,0 +1,4 @@
+linguagens = ["python", "js", "c", "java", "csharp"]
+
+print(linguagens.index("java"))  # 3
+print(linguagens.index("js"))  # 0
