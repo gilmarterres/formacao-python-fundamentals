8ffdc0021fe0187b16fa00bd4b3ffbecc03ed99f
Gilmar Ferreira Terres Correa <gilmarftc@gmail.com>
2026-01-16 22:42:37 -0300

    funçoes

--- 01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
+++ 01 - Estrutura de dados/05 - Funções/01_retorno_da_funcao.py
@@ -8,6 +8,10 @@ def retorna_antecessor_e_sucessor(numero):
 
     return antecessor, sucessor
 
+def func3():
+    a = 5
+    return a
 
 print(calcular_total([10, 20, 34]))  # 64
 print(retorna_antecessor_e_sucessor(10))  # (9, 11)
+print(func3())
