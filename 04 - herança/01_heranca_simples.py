ab4ad07860a0f395bd76e7ae3535becdcadf168e
Gilmar <gilmarftc@gmail.com>
2026-01-29 23:14:46 -0300

    hand on

---
+++ 04 - herança/01_heranca_simples.py
@@ -0,0 +1,17 @@
+class Veiculo:
+    def __init__(self):
+        pass
+    pass
+
+class Motocicleta(Veiculo):
+    pass
+
+class Carro(Veiculo):
+    pass
+
+class Caminhao(Veiculo):
+    pass
+
+
+
+# hand on 3min
