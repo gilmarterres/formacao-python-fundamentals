174fceafd832102833b8f1191c91988dac67bfb8
Gilmar Ferreira Terres Correa <gilmarftc@gmail.com>
2026-02-14 23:30:24 -0300

    herança

--- 04 - herança/01_heranca_simples.py
+++ 04 - herança/01_heranca_simples.py
@@ -1,7 +1,11 @@
 class Veiculo:
-    def __init__(self):
-        pass
-    pass
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+    
+    def ligar_motor(self):
+        print("Ligando o motor")
 
 class Motocicleta(Veiculo):
     pass
@@ -13,5 +17,13 @@ class Caminhao(Veiculo):
     pass
 
 
+moto = Motocicleta("preta", "abc-123", 2)
+moto.ligar_motor()
 
-# hand on 3min
+carro = Carro("branco", "anu1614", 4)
+carro.ligar_motor()
+
+caminhao = Caminhao("azul", "gdf8765", 16)
+
+
+##6.44
