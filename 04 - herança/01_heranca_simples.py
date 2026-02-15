2289b189d1bce315f97e770537178ebef212ddc9
Gilmar Ferreira Terres Correa <gilmarftc@gmail.com>
2026-02-14 23:49:49 -0300

    herança simples

--- 04 - herança/01_heranca_simples.py
+++ 04 - herança/01_heranca_simples.py
@@ -3,27 +3,35 @@ class Veiculo:
         self.cor = cor
         self.placa = placa
         self.numero_rodas = numero_rodas
-    
+
     def ligar_motor(self):
         print("Ligando o motor")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
 
 class Motocicleta(Veiculo):
     pass
 
+
 class Carro(Veiculo):
     pass
 
+
 class Caminhao(Veiculo):
-    pass
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
 
 
-moto = Motocicleta("preta", "abc-123", 2)
-moto.ligar_motor()
+moto = Motocicleta("preta", "abc-1234", 2)
+carro = Carro("branco", "xde-0098", 4)
+caminhao = Caminhao("roxo", "gfd-8712", 8, True)
 
-carro = Carro("branco", "anu1614", 4)
-carro.ligar_motor()
-
-caminhao = Caminhao("azul", "gdf8765", 16)
-
-
-##6.44
+print(moto)
+print(carro)
+print(caminhao)
