aa422c578d59d968e2d77b5c4aa5aa7758dbf9aa
Gilmar <gilmarftc@gmail.com>
2026-03-16 22:29:04 -0300

    desafios

---
+++ 09 - Classes Abstratas/desafio02.py
@@ -0,0 +1,18 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+
+#TODO: Crie um método para retornar as informações formatas com Nome e Idade:    
+    def exibir_informaçoes(self):
+        return f"Nome: {self.nome}, Idade: {self.idade}"
+
+# Entrada do usuário
+nome = input()
+idade = int(input())
+
+# TODO: Crie uma instância da pessoa:
+pessoa = Pessoa(nome, idade)
+
+#TODO: Chame o método para retornar as informações formatadas e imprima o resultado:
+print(pessoa.exibir_informaçoes())
---
+++ 09 - Classes Abstratas/desafio03.py
@@ -0,0 +1,15 @@
+#TODO: Crie uma classe para converter temperaturas de Celsius para Fahrenheit e um método que realiza o cálculo de conversão:
+class ConversorTemperatura:
+  def celsius_para_fahrenheit(self,celsius):
+    fahrenheit = (celsius * 9 / 5) + 32
+    return fahrenheit
+    
+
+# Entrada do usuário
+celsius = float(input())
+
+# TODO: Crie uma instância do conversor:
+conversor = ConversorTemperatura()
+
+fahrenheit = conversor.celsius_para_fahrenheit(celsius)
+print(fahrenheit)
