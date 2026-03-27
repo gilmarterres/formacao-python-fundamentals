f0cd6b9df24e9b6b393999c83c69715a3ccf886d
Gilmar <gilmarftc@gmail.com>
2026-03-27 12:57:13 -0300

    decorador introspecção

--- Decoradores_interadores/5_decorador_com_argumentos.py
+++ Decoradores_interadores/5_decorador_com_argumentos.py
@@ -1,18 +1,22 @@
 def meu_decorador(funcao):
     def envelope(*args, **kwargs):
         print("faz algo antes de executar")
-        funcao(*args, **kwargs)
+        resultado = funcao(*args, **kwargs)
         print("faz algo antes de executar")
+        return resultado
 
     return envelope
 
 @meu_decorador
-def ola_mundo():
-    print("Olá mundo")
+def ola_mundo(nome, outro_argumento):
+    print(f"Olá mundo {nome}!")
+    return nome.upper()
 
 
 ##ola_mundo = meu_decorador(ola_mundo)
-ola_mundo()
+resultado = ola_mundo("João", 1000)
+print(resultado)
+print(ola_mundo.__name__)
 
 
 
