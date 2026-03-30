339c7eb5fed626dc55da12cc86638e9e2d38b003
Gilmar Ferreira Terres Correa <gilmarftc@gmail.com>
2026-03-29 21:46:47 -0300

    desafio

---
+++ 09 - Classes Abstratas/desafio01.py
@@ -0,0 +1,13 @@
+# TODO: Crie uma classe e método para realizar a soma:
+class Calculadora:
+    def soma(self, a, b):
+        return a+b
+
+num1 = int(input())
+num2 = int(input())
+
+# Criando uma instância da calculadora
+calc = Calculadora()
+
+resultado = calc.soma(num1, num2)
+print(resultado)
